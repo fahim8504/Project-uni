7b808a5a58b1b073c7acc935e65f4dcdf9365753
Mohamed Fahim <m.mhdfahim@gmail.com>
2024-08-26 20:15:23 +0530

    changes in app.js

--- liveapp/src/App.js
+++ liveapp/src/App.js
@@ -1,18 +1,26 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Home from './components/Home';
-import ChatRoom from './components/ChatRoom';
-import Login from './components/Login';
+import { Route, Switch } from 'react-router-dom';
+import Home from './pages/Home';
+import College from './pages/College';
+import Department from './pages/Department';
+import ChatRoom from './components/Chat/ChatRoom';
+import Login from './components/Auth/Login';
+import Register from './components/Auth/Register';
+import Navbar from './components/Layout/Navbar';
 
 function App() {
   return (
-    <Router>
+    <div>
+      <Navbar />
       <Switch>
         <Route path="/" exact component={Home} />
         <Route path="/login" component={Login} />
-        <Route path="/chat/:college/:department/:year" component={ChatRoom} />
+        <Route path="/register" component={Register} />
+        <Route path="/college" component={College} />
+        <Route path="/department" component={Department} />
+        <Route path="/chatroom/:college/:department/:year" component={ChatRoom} />
       </Switch>
-    </Router>
+    </div>
   );
 }
 
